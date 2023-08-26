71613d89a75029c96266c261b13ddf50da829d58
ejmat <ejmat@ethans-air-2.lan>
2023-08-26 17:25:36 -0400

    adding instructions

--- computerScience/src/computerScience/petAdvice.java
+++ computerScience/src/computerScience/petAdvice.java
@@ -7,11 +7,16 @@ public class petAdvice {
 	private static Scanner scan = new Scanner(System.in);
 	private static petAdvice pet = new petAdvice();
 
-	
 	// gets current location of person
+	private static void instructions() {
+		System.out.println("Instructions:");
+		System.out.println("enter the first letter of the choice you make - ex. if you choose { A) apartment } enter the first letter shown");
+		System.out.println("");
+	}
+	
 	private char getLocation() {
-		System.out.println("Do you live in a:");
-		System.out.println("A) an apartment");
+		System.out.println("Do you live in a(n):");
+		System.out.println("A) apartment");
 		System.out.println("H) house");
 		System.out.println("D) dormitory");
 
@@ -24,7 +29,6 @@ public class petAdvice {
 
 	// gets hours worked by person
 	private char getHours() {
-		Scanner scanHours = new Scanner(System.in);
 		System.out.println("How many hours are you at home per day:");
 		System.out.println("A) 18 or more");
 		System.out.println("B) 10 to 17");
@@ -32,10 +36,10 @@ public class petAdvice {
 		System.out.println("D) 6 to 7");
 		System.out.println("E) 0 to 5");
 
-		String input = scanHours.nextLine();
+		String input = scan.nextLine();
 		char hoursLower = input.charAt(0);
 		char hoursUpper = Character.toUpperCase(hoursLower);
-		scanHours.close();
+		scan.close();
 		return hoursUpper;
 
 	}
@@ -63,7 +67,7 @@ public class petAdvice {
 
 		}
 		if (location == 'D') {
-			if (hours == 'A' || hours == 'B' || hours == 'E' || hours == 'D') {
+			if (hours == 'A' || hours == 'B' || hours == 'C' || hours == 'D') {
 				return "Fish";
 			}
 			if (hours == 'E') {
@@ -74,17 +78,17 @@ public class petAdvice {
 
 		return "Incorrect Inputs";
 	}
-	
+
 	public String getReccomendation() {
+		pet.instructions();
 		char location = pet.getLocation();
 		char hours = pet.getHours();
 		String output = pet.logic(location, hours);
-		
+
 		return output;
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println(pet.getReccomendation());
 	}
 }
